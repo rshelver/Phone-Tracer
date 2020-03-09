05f8ec40c5da8a531e0bf40af14c3820a7652a5a
rshelver <rshelver@outlook.com>
2020-03-08 23:51:41 -0700

    Updated to 1.3.0 beta 5
    prepared for finding names

--- Phone-Tracer.py
+++ Phone-Tracer.py
@@ -9,14 +9,77 @@ import webbrowser
 from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H%m")
+valid_confirm_loop = "null"
 
 low_risk_check = False
 medium_risk_check = False
 high_risk_check = False
 unknow_risk_level = True
 
+validStatus = 0 # 0 = null, 1 = Valid in-state, 2 = international unconfirmed.
 
 
+def checkInternational ():
+    if country_code != "+1":
+        print("International code detected")
+        validStatus = 2
+    if country_code == "+1":
+        print("Invalid Number")
+        input("Press Enter to Exit...")
+        quit("Invalid Number")
+
+
+def getRiskLevel():
+    if low_risk_check == True:
+        print("[+] This number has a low Risk")
+
+    #if medium_risk_check == True:
+    #    print("[+] This number has a medium risk (Use caution)")
+
+    if high_risk_check == True:
+        print("[X] This number is High Risk")
+
+    if unknow_risk_level == True:
+        print("[Error] Unable to get risk level")
+
+
+
+
+
+
+def write(validStatus):
+    try:
+        if validStatus == 1:
+            f = open("PhoneTrace.txt", "a+")
+            f.write("\n\n\n\n" + country_code +" "+ phone_number)
+            f.write("\nNumber is Valid")
+            f.write("\nCountry Prefix: " + country_prefix)
+            f.write("\nCountry Code:" + country_code_out)
+            f.write("\nCountry Name: " + country_name)
+            f.write("\nCity: " + city)
+            f.write("\nCarrier: " + carrier)
+            f.write("\nLandlane or Mobile: " + cell_type)
+        if validStatus != 0:
+            if low_risk_check == True:
+                f.write("\n[+]This number is low risk")
+
+            if medium_risk_check == True:
+                f.write("\n[]This number is medium risk")
+
+            if high_risk_check == True:
+                f.write("\n[X] This number is high risk")
+
+            if unknow_risk_level == True:
+                f.write("\n[Error] Unable to get risk level")
+        timeout(1.5)
+        f.close()
+    except FileExistsError:
+        print("FileExistsError occured - if you get this error please report it on my github")
+        #fileOverwrite = input("File already exists, do you wish to overwrite? (Y/N): ")
+        #if fileOverwrite == "y":
+        #    os.remove("PhoneTrace.txt")
+        #if fileOverwrite == "n":
+        #    input("Press Enter to close...")
 
 
 
@@ -34,9 +97,11 @@ def clear():
 
 
 #intro
-print("Phone Tracer 1.2")
+clear()
+print("Phone Tracer 1.3.0b5")
 print("Developed by Mutiny27")
 print("Note: The spam caller feature is in beta and may not provide fully accurate information")
+#print("Note: The name finder feature may not be 100% accurate, In testing it's been found to be mostly accurate")
 print("Press Enter to continue...")
 input()
 clear()
@@ -75,26 +140,26 @@ if main_menu_choice=='1':
     valid_check=response.text[9:13]
     if valid_check=='true':
         valid_confirm_loop='valid'
+        validStatus = 1
     elif valid_check!='true':
-        print("Number is Invalid")
-        input("Press Enter to Continue...")
+        checkInternational()
 
 
     #check scammer
-    scamCheckURL = 'https://spamcalls.net/us-en/search?q=%2B' + country_code + phone_number
+    scamCheckURL = 'https://spamcalls.net/us-en/number/' + country_code + phone_number
     scamresponse = requests.get(scamCheckURL)
     if "spam risk is low<" in scamresponse.text:
         low_risk_check = True
         unknow_risk_level = False
 
 
-    #if "Neutral" in scamresponse.text and low_risk_check == False:
-    #    medium_risk_check = True
-    #    low_risk_check = False
-    #    unknow_risk_level = False
+    if "&rating=Probably+Spam" in scamresponse.text and low_risk_check == False:
+        medium_risk_check = True
+        low_risk_check = False
+        unknow_risk_level = False
 
 
-    if  "Scam Call:" in scamresponse.text and low_risk_check == False:
+    if  "&rating=Spam" in scamresponse.text and low_risk_check == False:
         high_risk_check = True
         low_risk_check = False
         medium_risk_check = False
@@ -111,7 +176,8 @@ city = response_json.get('location', "")
 carrier = response_json.get('carrier', "")
 cell_type = response_json.get('line_type', "")
 
-
+if "Sprint" in carrier:
+    carrier = "Tmobile"
 
 
 
@@ -125,48 +191,18 @@ if valid_confirm_loop=='valid':
     print("City: " + city)
     print("Carrier: " + carrier)
     print("Landlane or Mobile: " + cell_type)
-    if low_risk_check == True:
-        print("[+] This number has a low Risk")
-
-    if medium_risk_check == True:
-        print("[+] This number has a medium risk (Use caution)")
-
-    if high_risk_check == True:
-        print("[+] This number is Risky")
-
-    if unknow_risk_level == True:
-        print("[X] Unable to get risk level")
+    getRiskLevel()
 
 
 
     save = input("Would you like this saved to a file? (Y/N): ")
 
     if save == "y":
-        try:
-            f = open("PhoneTrace"+city+country_code+".txt", "w+")
-            f.write(country_code+phone_number)
-            f.write("\nNumber is Valid")
-            f.write("\nCountry Prefix: " + country_prefix)
-            f.write("\nCountry Code:" + country_code_out)
-            f.write("\nCountry Name: " + country_name)
-            f.write("\nCity: " + city)
-            f.write("\nCarrier: " + carrier)
-            f.write("\nLandlane or Mobile: " + cell_type)
-            if low_risk_check == True:
-                f.write("\n[+]This number is low risk")
+        write(validStatus)
 
-            if medium_risk_check == True:
-                f.write("\n[]This number is medium risk")
 
-            if high_risk_check == True:
-                f.write("\nThis number is high risk")
 
-            if unknow_risk_level == True:
-                f.write("\n[X] Unable to get risk level")
-            timeout(1.5)
-            f.close()
-        except FileExistsError:
-            print("You've already scanned this number, no need to save it")
+
 
 
 
